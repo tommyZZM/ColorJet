77f157050c6d07b4f9b17245e80377af0f88652c
tommyZZM <452490446@qq.com>
2015-05-27 10:08:59 +0800

    clouds

--- src/game/scenery/objs/Cloud.ts
+++ src/game/scenery/objs/Cloud.ts
@@ -2,18 +2,29 @@
  * Created by tommyZZM on 2015/5/22.
  */
 module game{
+    import TextureRepository = alcedo.canvas.TextureRepository;
+
     export class Cloud extends Entity{
         public velocitystatic= true;
 
         private _cloud:any
+        private _clouddebug:any
 
         public constructor(width:number,height:number){
             super(new canvas.DisplatObjectContainer());
-            this.display.alpha = 0.8;
-            this._cloud = new canvas.graphic.Rectangle(width,height);
+            this._cloud = new canvas.Sprite((<TextureRepository>alcedo.core(TextureRepository)).get("darkcloud"));
+            this._cloud.width = width;
+            this._cloud.height = height;
+            this._cloud.texture._textureWidth = width;
+            this._cloud.texture._textureHeight = height;
+
             this.display.addChild(this._cloud);
-            this.display.width = this._cloud.width;
-            this.display.height = this._cloud.height;
+
+            this._clouddebug = new canvas.graphic.Rectangle(width,height);
+            this.display.addChild(this._clouddebug);
+            this.display.width = this._clouddebug.width;
+            this.display.height = this._clouddebug.height;
+            this._clouddebug.alpha = 0.2;
 
             this._body = (new sat.Box(new sat.Vector(0,0),this.display.width
                 ,this.display.height)).toPolygon();
@@ -22,12 +33,11 @@ module game{
         }
 
         public active(){
-            //trace("active");
-            this._cloud.fillcolour = "#f1c40f";
+            this._clouddebug.fillcolour = "#f1c40f";
         }
 
         public disactive(){
-            this._cloud.fillcolour = "#3498db";
+            this._clouddebug.fillcolour = "#3498db";
         }
     }
 }
